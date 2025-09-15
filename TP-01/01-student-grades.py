758bc8a6b02c83cc7bf37e495bf00f031603f6d7
Lumo27 <lucastmotta27@gmail.com>
2025-09-15 20:48:59 -0300

    add counts to know final results

--- TP-01/01-student-grades.py
+++ TP-01/01-student-grades.py
@@ -1,5 +1,6 @@
 examGrade = 0.0
 totalGrades = 0.0
+count1,count2,count3=0,0,0
 for x in range (0,58):
     print("Ingrese su nota, debe ser entre 0 y 10")
     examGrade= float(input("Nota: "))
@@ -11,10 +12,13 @@ for x in range (0,58):
     totalGrades= totalGrades + examGrade
     if examGrade < 4:
           print("Alumno desaprobado")
+          count1= count1 + 1 
     elif examGrade >=4 and examGrade <7:
           print("Alumno a recuperatorio")
+          count2= count2 + 1
     else:
           print("Alumno aprobado")
+          count3= count3 + 1
 
 print("El promedio final de las notas fue de: ", totalGrades/5)
-    
+print("Resultados finales, desaprobados: ", count1, ", recuperatorios: ", count2, ", aprobados: ", count3)
