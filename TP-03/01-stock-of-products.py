7c372bf2d2b3a27f5a419f97431826915eb83f4d
Lumo27 <lucastmotta27@gmail.com>
2025-09-16 16:41:16 -0300

    Add product information processing script for cookie factory

---
+++ TP-03/01-stock-of-products.py
@@ -0,0 +1,12 @@
+# Se desea procesar la información de los productos de una fábrica de galletitas, para ello se
+# lee de cada producto, su nombre:
+#  Se sabe que el último producto, que NO se procesa, es “ZZZZZ”
+#  Se desea obtener la siguiente información:
+# o La cantidad de productos.
+count = 0
+print("Ingrese el nombre del producto (Para finalizar ingrese 'ZZZZZ'): ")
+product= input("Nombre del producto: ")
+while product != "ZZZZZ":
+    count += 1
+    product=input("Ingrese el nombre de otro producto (Para finalizar ingrese 'ZZZZZ'): ")
+print("La cantidad de productos es: ", count)
